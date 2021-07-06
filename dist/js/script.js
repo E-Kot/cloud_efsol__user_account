42c78e52b11f1ab824a430fc8cc617d531f5991e
E-Kot <eva.k.web@gmail.com>
2021-07-06 23:19:54 +0300

    Added: Filter functionality and style.

--- dist/js/script.js
+++ dist/js/script.js
@@ -12387,3 +12387,28 @@ testWebP(function (support) {
 
 })));
 
+
+// Adding category name (attribute value) to page url and page title
+
+(function () {
+    let filterNav = document.querySelectorAll('.filter-nav');
+    let baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+
+    for (var i = 0; i < filterNav.length; i++) {
+        filterNav[i].onclick = function(event) {
+            let attribute = this.querySelector('a[data-add-url]').dataset.addUrl;
+            let category = this.querySelector('a[data-category-title]').dataset.categoryTitle;
+
+            if (history.pushState) {
+                let newUrl = baseUrl + '?' + attribute;
+                history.pushState(null, null, newUrl);
+            }
+            document.getElementById("category-title").innerHTML = category;
+        }
+    }
+    
+}());
+
+
+
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
 // including script files
 @@include('test_web_p.js')
 @@include('uikit.js')
+@@include('custom.js')
