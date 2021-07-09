5cccbc1800e34a7657a3e294edfb816999a3738e
E-Kot <eva.k.web@gmail.com>
2021-07-09 12:20:16 +0300

    Fixed: form, error in js file.

--- dist/js/script.js
+++ dist/js/script.js
@@ -12468,7 +12468,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let filterNav = document.querySelectorAll('.filter-nav');
     let baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
 
-    for (var i = 0; i < filterNav.length; i++) {
+    for (let i = 0; i < filterNav.length; i++) {
         filterNav[i].onclick = function(event) {
             let attribute = this.querySelector('a[data-add-url]').dataset.addUrl;
             let category = this.querySelector('a[data-category-title]').dataset.categoryTitle;
@@ -12491,20 +12491,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let el1 = document.querySelectorAll('.accordion-item');
 
     for (let i = 0; i < el1.length; i++) {
-        el1[i].onclick = function(event) {
+        el1[i].onclick = function() {
             let ele = this.querySelector('.uk-open .service-form');
-
-            if(ele.addEventListener){
+            if(ele) {
                 ele.addEventListener("submit", callback, false);  //Modern browsers
-            }else if(ele.attachEvent){
-                ele.attachEvent('onsubmit', callback);            //Old IE
             }
-            document.querySelector(".accordion-item.uk-open .service-form").addEventListener("submit", function(e){
-                if(!isValid){
-                    e.preventDefault();    //stop form from submitting
-                }
-            });
-
             function callback() {
                 UIkit.modal('#modal__callback-service-form').show();
             }
